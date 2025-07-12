64a63f59dbd989eda6b5fffa6d366601cb16a2a0
KTBates <bates.kaylen@gmail.com>
2025-07-12 12:33:01 -0500

    Initial Commit

---
+++ src/main/java/com/squirrelly_app/file_manager/FileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.squirrelly_app.file_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerApplication.class, args);
+	}
+
+}
